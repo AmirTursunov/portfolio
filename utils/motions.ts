68649d2224191a2e2c476c3ab243b18b5dab3e88
MacBookUz <amirtursunov2@gmail.com>
2025-08-15 00:29:48 +0500

    motion is changed

--- utils/motions.ts
+++ utils/motions.ts
@@ -1,7 +1,7 @@
 // Sidebar
 export const sidebar = {
   hidden: { x: '-100%', opacity: 0 },
-  visible: { x: 0, opacity: 1, transition: { duration: 0.8, type: 'tween' } },
+  visible: { x: 0, opacity: 1, transition: { duration: 0.5, type: 'tween' } },
 };
 
 // Home Section
